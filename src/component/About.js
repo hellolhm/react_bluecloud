e774efd48de96d98984ea3a8bbecc74e8bf37f1f
lettuce96 <surhyst123@gmail.com>
2023-01-19 09:57:40 +0900

    파비콘과폰트수정

--- src/component/About.js
+++ src/component/About.js
@@ -13,14 +13,14 @@ function About() {
                 </div>
                 <div className='text-center fs-4'>
                     <p className={`fs-1 pt-5 pb-lg-5 container`}>저희 청운 한의원은</p>
-                    <p> 전 직원 마스크 착용 및 손소독을 철저히 하며<br></br>
+                    <p className='fs-5'> 전 직원 마스크 착용 및 손소독을 철저히 하며<br></br>
                         접수 시 손소독과 체온 측정 뒤에 입장가능합니다
                     </p> 
-                    <p>
+                    <p className='fs-5'>
                         청운 한의원에서는 마스크를 두고 오신<br></br> 환자분들을 위해
                         일회용 마스크를 항상 <br></br>구비하고 있습니다.<br></br>
                     </p>
-                    <p>
+                    <p className='fs-5'>
                         베드 소독 또한 수시로 진행하고 있으며, <br></br>
                         침, 사혈 부항, 주사기는 1회용으로 <br></br>안심하시고 치료받으실 수 있습니다.
                     </p>
